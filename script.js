cb92a390137d6eb9298b7665173ac00d17aaed04
GOPI CHAND <gopichandnaidu65@gmail.com>
2024-01-24 16:40:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// function validateForm() {
+//     var username = document.getElementById("username").value;
+//     var password = document.getElementById("password").value;
+
+//     // Simple validation - you may want to implement more secure methods
+//  //   if (username === "your_username" && password === "your_password") {
+//         alert("Login successful!");
+//         return true;
+//     } else {
+//         alert("Invalid username or password. Please try again.");
+//         return false;
+//     }
+// }
+function display() {
+    // You may perform login validation here
+    // If login is successful, redirect to the homepage
+    window.location.href = "Homepage.html";
+  //  return false; // To prevent form submission
+}
